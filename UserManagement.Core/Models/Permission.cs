29d9a36760e4612e3437f41a4626a5c8d2923e7c
Emin <eminus.prime@gmail.com>
2025-06-08 17:24:14 +0200

    Update Permission entity with const fields

--- UserManagement.Core/Models/Permission.cs
+++ UserManagement.Core/Models/Permission.cs
@@ -4,6 +4,8 @@ namespace UserManagement.Core.Models;
 
 public class Permission : IEntity
 {
+    public const int MAX_NAME_LENGTH = 36;
+    public const int MAX_DESCRIPTION_LENGTH = 256;
     public Guid Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string Description { get; set; } = String.Empty;
