cfed2678161316c53fe63f7dbf737fde815b9193
Emin <eminus.prime@gmail.com>
2025-06-08 17:33:06 +0200

    Add getUserByEmail impl

--- UserManagement.Infrastructure/Postgres/Repositories/UserRepository.cs
+++ UserManagement.Infrastructure/Postgres/Repositories/UserRepository.cs
@@ -7,5 +7,19 @@ namespace UserManagement.Infrastructure.Postgres.Repositories;
 
 public class UserRepository: GenericRepository<User>, IUserRepository
 {
-    public UserRepository(PostgresContext context) : base(context) { }
+    private readonly DbContext _context;
+
+    public UserRepository(PostgresContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        var user = await _context.Set<User>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == email);
+        
+        return user;
+    }
 }
