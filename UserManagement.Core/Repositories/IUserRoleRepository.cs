3fe6729b9ef024a3c154fffd1d1018179843ab23
Emin <eminus.prime@gmail.com>
2025-06-11 16:22:14 +0200

    Update UserRoleRepository

--- UserManagement.Core/Repositories/IUserRoleRepository.cs
+++ UserManagement.Core/Repositories/IUserRoleRepository.cs
@@ -4,5 +4,7 @@ namespace UserManagement.Core.Repositories;
 
 public interface IUserRoleRepository
 {
-    public Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId);
+    public Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId, CancellationToken cancellationToken);
+    public Task AddRoleUserAsync(UserRole userRole, CancellationToken cancellationToken);
+    public Task DeleteRoleUserAsync(UserRole userRole, CancellationToken cancellationToken);
 }
--- UserManagement.Infrastructure/Postgres/Repositories/UserRoleRepository.cs
+++ UserManagement.Infrastructure/Postgres/Repositories/UserRoleRepository.cs
@@ -6,14 +6,26 @@ namespace UserManagement.Infrastructure.Postgres.Repositories;
 
 public class UserRoleRepository(PostgresContext context): IUserRoleRepository
 {
-    public async Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId)
+    public async Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId, CancellationToken cancellationToken)
     {
         var roles = await context.Set<UserRole>()
             .Include(ur => ur.User)
             .Where(ur => ur.UserId == userId)
             .Select(ur => ur.Role)
-            .ToListAsync();
+            .ToListAsync(cancellationToken);
         
         return roles;   
+    }
+
+    public async Task AddRoleUserAsync(UserRole userRole, CancellationToken cancellationToken)
+    {   
+        context.Set<UserRole>().Add(userRole);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteRoleUserAsync(UserRole userRole, CancellationToken cancellationToken)
+    {
+        context.Set<UserRole>().Remove(userRole);
+        await context.SaveChangesAsync(cancellationToken);
     }
 }
