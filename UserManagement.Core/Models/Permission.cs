bd2970fd2ea883330c40fd075c4936f39598ee47
Emin <eminus.prime@gmail.com>
2025-06-11 10:16:17 +0200

    Update Permission.cs get rid of timestamps

--- UserManagement.Core/Models/Permission.cs
+++ UserManagement.Core/Models/Permission.cs
@@ -9,7 +9,5 @@ public class Permission
     public int Id { get; set; }
     public string Name { get; set; } = String.Empty;
     public string Description { get; set; } = String.Empty;
-    public bool IsEnabled { get; set; }
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-    public DateTimeOffset UpdatedAt { get; set; }
+    public bool IsEnabled { get; set; } = true;
 }
