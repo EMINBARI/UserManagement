6207f65310446cf844c2081d9931d5d57740798b
Emin <eminus.prime@gmail.com>
2025-06-11 09:49:10 +0200

    Update Role, Permission and RolePermission config

--- UserManagement.Infrastructure/Config/RoleConfig.cs
+++ UserManagement.Infrastructure/Config/RoleConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UserManagement.Core.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserManagement.Core.Enums;
 
 namespace UserManagement.Infrastructure.Config
 {
@@ -15,8 +16,20 @@ namespace UserManagement.Infrastructure.Config
                 .IsRequired();
 
             builder.Property(r => r.Description) 
-                .HasMaxLength(Role.MAX_DESCRIPTION_LENGTH)
-                .IsRequired();
+                .HasMaxLength(Role.MAX_DESCRIPTION_LENGTH);
+
+            var roles = Enum
+                .GetValues<RoleType>()
+                .Select(r => new Role
+                {
+                    Id = (int)r,
+                    Name = r.ToString(),
+                    Description = $"Some {r.ToString()} description"
+                    
+                });
+            
+            builder.HasData(roles);
         }
+        
     }
 }
--- UserManagement.Infrastructure/Config/RolePermissionConfig.cs
+++ UserManagement.Infrastructure/Config/RolePermissionConfig.cs
@@ -1,11 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserManagement.Core.Enums;
 using UserManagement.Core.Models;
+using UserManagement.Infrastructure.Authorization;
+using UserManagement.Infrastructure.Authorization.Configuration;
 
 namespace UserManagement.Infrastructure.Config;
 
 public class RolePermissionConfig: IEntityTypeConfiguration<RolePermission>
 {
+    private readonly RolePermissionsOptions _rolePermissionsOptions;
+ 
+    public RolePermissionConfig(RolePermissionsOptions rolePermissionsOptions)
+    {
+        _rolePermissionsOptions = rolePermissionsOptions;
+    }
     public void Configure(EntityTypeBuilder<RolePermission> builder)
     {
         builder.HasKey(rp => new { rp.PermissionId, rp.RoleId });
@@ -17,5 +26,17 @@ public class RolePermissionConfig: IEntityTypeConfiguration<RolePermission>
         builder.HasOne(rp => rp.Permission)
             .WithMany()
             .HasForeignKey(rp => rp.PermissionId);
+
+        builder.HasData(ParseRolePermissions());
+    }
+
+    private RolePermission[] ParseRolePermissions()
+    {
+        return _rolePermissionsOptions.Mappings
+            .SelectMany(rp => rp.Rermissions
+                .Select(p => new RolePermission{
+                    RoleId = (int)Enum.Parse<RoleType>(rp.Role),
+                    PermissionId = (int)Enum.Parse<PermissionCategory>(p)
+                })).ToArray();
     }
 }
